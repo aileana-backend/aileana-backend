6fb717c8f6dfed47cde6d236f34d35c950ef677d
rankyakab <rankyakab@gmail.com>
2025-08-27 18:24:57 +0100

    wallet added

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,35 +34,36 @@ const signup = async (req, res) => {
     await user.save();
 
     // // Wallet payload for OnePipe
-    // const walletUserData = {
-    //   _id: user._id,
-    //   customer_ref: `user_${user._id}`,
-    //   firstname,
-    //   surname,
-    //   email,
-    //   mobile_no: phone,
-    //   provider_code: PROVIDER_CODE,
-    //   provider_name: PROVIDER_NAME,
-    //   account_type: "static",
-    // };
+    const walletUserData = {
+      _id: user._id,
+      customer_ref: `user_${user._id}`,
+      first_name,
+      middle_name,
+      last_name,
+      email,
+      //mobile_no: phone,
+      provider_code: PROVIDER_CODE,
+      provider_name: PROVIDER_NAME,
+      account_type: "static",
+    };
 
     // // Create wallet
-    // const walletResponse = await createWallet(walletUserData);
+    const walletResponse = await createWallet(walletUserData);
 
-    // if (walletResponse.status === "Successful") {
-    //   const existingWallet = await Wallet.findOne({ user: walletUserData._id });
-    //   if (!existingWallet) {
-    //     const wallet = new Wallet({
-    //       user: user._id,
-    //       externalId:
-    //         walletResponse.data?.provider_response?.account_number ||
-    //         walletResponse.data?.externalId,
-    //       balance: 0,
-    //       currency: walletResponse.data?.provider_response?.currency || "NGN",
-    //     });
-    //     await wallet.save();
-    //   }
-    // }
+    if (walletResponse.status === "Successful") {
+      const existingWallet = await Wallet.findOne({ user: walletUserData._id });
+      if (!existingWallet) {
+        const wallet = new Wallet({
+          user: user._id,
+          externalId:
+            walletResponse.data?.provider_response?.account_number ||
+            walletResponse.data?.externalId,
+          balance: 0,
+          currency: walletResponse.data?.provider_response?.currency || "NGN",
+        });
+        await wallet.save();
+      }
+    }
 
     // Generate token
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
@@ -89,34 +90,34 @@ const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
 
-    // let wallet = await Wallet.findOne({ user: user._id });
-    // if (!wallet) {
-    //   const walletUserData = {
-    //     _id: user._id,
-    //     customer_ref: `user_${user._id}`,
-    //     firstname: user.firstname,
-    //     surname: user.surname,
-    //     email: user.email,
-    //     mobile_no: user.phone,
-    //     provider_code: PROVIDER_CODE,
-    //     provider_name: PROVIDER_NAME,
-    //     account_type: "static",
-    //   };
+    let wallet = await Wallet.findOne({ user: user._id });
+    if (!wallet) {
+      const walletUserData = {
+        _id: user._id,
+        customer_ref: `user_${user._id}`,
+        firstname: user.firstname,
+        surname: user.surname,
+        email: user.email,
+        mobile_no: user.phone,
+        provider_code: PROVIDER_CODE,
+        provider_name: PROVIDER_NAME,
+        account_type: "static",
+      };
 
-    //   const walletResponse = await createWallet(walletUserData);
+      const walletResponse = await createWallet(walletUserData);
 
-    //   if (walletResponse.status === "Successful") {
-    //     wallet = new Wallet({
-    //       user: user._id,
-    //       externalId:
-    //         walletResponse.data?.provider_response?.account_number ||
-    //         walletResponse.data?.externalId,
-    //       balance: 0,
-    //       currency: walletResponse.data?.provider_response?.currency || "NGN",
-    //     });
-    //     await wallet.save();
-    //   }
-    // }
+      if (walletResponse.status === "Successful") {
+        wallet = new Wallet({
+          user: user._id,
+          externalId:
+            walletResponse.data?.provider_response?.account_number ||
+            walletResponse.data?.externalId,
+          balance: 0,
+          currency: walletResponse.data?.provider_response?.currency || "NGN",
+        });
+        await wallet.save();
+      }
+    }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRES_IN || "7d",
