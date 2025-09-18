833ab50ce91cba2d2d477d68c31f4b1e5e3b3d97
Obi Pascal <pascalobi83@gmail.com>
2025-09-18 12:38:01 +0100

    add prisma schema and wallet model; update dependencies for prisma client

--- models/Wallet.js
+++ models/Wallet.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const walletSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-      unique: true,
-    },
-    nairaBalance: { type: Number, default: 0 },
-    credixBalance: { type: Number, default: 0 },
-    tradebitBalance: { type: Number, default: 0 },
-  },
-  { timestamps: true }
-);
+	{
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+			unique: true,
+		},
+		nairaBalance: { type: Number, default: 0 },
+		credixBalance: { type: Number, default: 0 },
+		tradebitBalance: { type: Number, default: 0 },
+	},
+	{ timestamps: true },
+)
 
-module.exports = mongoose.model("Wallet", walletSchema);
+module.exports = mongoose.model("Wallet", walletSchema)
