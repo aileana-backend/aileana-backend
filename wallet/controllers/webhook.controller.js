273a15333da2bab401fa072fc1edd6eda7195bb8
Obi Pascal <pascalobi83@gmail.com>
2025-10-03 22:55:11 +0100

    feat: enhance webhook handling for successful transactions; add user wallet crediting and logging for webhook events

--- wallet/controllers/webhook.controller.js
+++ wallet/controllers/webhook.controller.js
@@ -1,5 +1,7 @@
 const logActivity = require("../../utils/activityLogger")
 const { verifyWebhook } = require("../../utils/webhookVerifyer")
+const transactionService = require("../services/transaction.service")
+const walletService = require("../services/wallet.service")
 
 const handleWebhookRequest = async (req, res) => {
 	try {
@@ -21,27 +23,64 @@ const handleWebhookRequest = async (req, res) => {
 
 		// Validate the signature
 		if (!isValid) {
-			await logActivity({
-				userId: null,
-				action: "webhook_unauthorized",
-				description: "Unauthorized webhook request due to invalid signature",
-				req,
-			})
 			return res.status(401).json({ msg: "Unauthorized" })
 		}
 
-		// Process the webhook event
-		await logActivity({
-			userId: null,
-			action: "webhook_event_received",
-			description: `Webhook event received: ${event.type}`,
-			req,
-			event,
-		})
+		// Let's get the event data and type
+		const eventData = event.eventData || false
+		const eventType = event.eventType || false
+		if (!eventData || !eventType) {
+			return res.status(400).json({ msg: "Invalid webhook data" })
+		}
 
-		console.log("====================================")
-		console.log(event)
-		console.log("====================================")
+		switch (eventType) {
+			// handle user wallet top-up via monnify dedicated account
+			case "SUCCESSFUL_TRANSACTION":
+				// Get user ID
+				const userId = eventData.product?.reference?.replace("AILEANA_", "") || null
+				if (!userId) {
+					return res.status(400).json({ msg: "Invalid user reference in webhook data" })
+				}
+
+				// Get the user wallet
+				const userWallet = await walletService.findByUserId(userId)
+				if (!userWallet) {
+					return res.status(404).json({ msg: "User wallet not found" })
+				}
+
+				// Check if transaction is already processed
+				const existingTransaction = await transactionService.getTransactionByReference(eventData.transactionReference)
+				if (existingTransaction) {
+					return res.status(200).json({ msg: "Transaction already processed" })
+				}
+
+				// Credit user wallet
+				const charges = Number(eventData?.amountPaid) - Number(eventData?.settlementAmount)
+				const toBeCredited = Number(eventData?.settlementAmount)
+				const creditTrnx = await walletService.creditWallet(userWallet.id, toBeCredited, charges, eventData?.transactionReference, "Wallet Top-up", {
+					rawEventData: eventData,
+					creditedBy: "monnify_webhook",
+				})
+				if (!creditTrnx) {
+					await logActivity({
+						userId,
+						action: "wallet_credit_failed",
+						description: `Failed to credit user wallet via webhook. Reference: ${eventData.transactionReference}`,
+						req,
+						event,
+					})
+					return res.status(500).json({ msg: "Failed to credit user wallet" })
+				}
+
+				await logActivity({
+					userId,
+					action: "wallet_credited",
+					description: `User wallet credited with NGN${toBeCredited} via webhook. Reference: ${eventData.transactionReference}`,
+					req,
+					event,
+				})
+				break
+		}
 
 		res.status(200).json({ msg: "Webhook processed successfully" })
 	} catch (error) {
